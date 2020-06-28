7d1bbc64cf5695aec4bd50f3d7b3e5fed635056c
rupam sahriar <rupamsahriar@gmail.com>
2020-06-28 12:04:16 +0600

    Survey Webhook add

--- server/routes/surveyRoute.js
+++ server/routes/surveyRoute.js
@@ -4,11 +4,70 @@ const requireCredits = require('../middlewares/requireCredit')
 const Survey = mongoose.model('surveys');
 const Mailer = require('../services/Mailer')
 const surveyTemplate = require('../services/emailTemplate/surveyTemplates')
+const _ = require('lodash')
+const { Path } = require('path-parser');
+const { URL } = require('url')
 
 module.exports = app => {
     app.get('/api/surveys/thanks', (req,res)=> {
         res.send('Thanks for voting!')
     })
+
+    // app.post('/api/surveys/webhooks', (req,res)=>{
+    //   console.log(req.body);
+    //   const p = new Path('/api/surveys/:surveyId/:choice')
+
+    //     const events = _.map(req.body, (event)=>{
+    //       const pathname = new URL(event.url).pathname
+    //       const match = p.test(pathname)
+
+    //       if(match) {
+    //         return {
+    //           email: event.email,
+    //           surveyId: match.surveyId,
+    //           choice: match.choice
+    //         }
+    //       }
+    //    })
+
+    //    console.log(events)
+    //    const compactEvent = _.compact(events);
+    //    const uniqueEvents = _.unionBy(compactEvent, 'email', 'surveyId');
+    //     console.log(uniqueEvents)
+    //   })
+
+
+    app.post('/api/surveys/webhooks', (req, res) => {
+      const p = new Path('/api/surveys/:surveyId/:choice');
+  
+      _.chain(req.body)
+        .map(({ email, url }) => {
+          const match = p.test(new URL(url).pathname);
+          if (match) {
+            return { email, surveyId: match.surveyId, choice: match.choice };
+          }
+        })
+        .compact()
+        .uniqBy('email', 'surveyId')
+        .each(({ surveyId, email, choice }) => {
+          Survey.updateOne(
+            {
+              _id: surveyId,
+              recipients: {
+                $elemMatch: { email: email, responded: false }
+              }
+            },
+            {
+              $inc: { [choice]: 1 },
+              $set: { 'recipients.$.responded': true },
+              lastResponded: new Date()
+            }
+          ).exec();
+        })
+        .value();
+  
+      res.send({});
+    });
 
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
       const { title, subject, body, recipients } = req.body;
