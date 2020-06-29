9a6192ecf0bafbd34023fbcd9b4ab41ebf932ff3
rupam sahriar <rupamsahriar@gmail.com>
2020-06-29 11:33:21 +0600

    complete

--- server/routes/surveyRoute.js
+++ server/routes/surveyRoute.js
@@ -49,37 +49,37 @@ module.exports = app => {
     //   })
 
 
-    // app.post('/api/surveys/webhooks', (req, res) => {
-    //   const p = new Path('/api/surveys/:surveyId/:choice');
+    app.post('/api/surveys/webhooks', (req, res) => {
+      const p = new Path('/api/surveys/:surveyId/:choice');
   
-    //   _.chain(req.body)
-    //     .map(({ email, url }) => {
-    //       const match = p.test(new URL(url).pathname);
-    //       if (match) {
-    //         return { email, surveyId: match.surveyId, choice: match.choice };
-    //       }
-    //     })
-    //     .compact()
-    //     .uniqBy('email', 'surveyId')
-    //     .each(({ surveyId, email, choice }) => {
-    //       Survey.updateOne(
-    //         {
-    //           _id: surveyId,
-    //           recipients: {
-    //             $elemMatch: { email: email, responded: false }
-    //           }
-    //         },
-    //         {
-    //           $inc: { [choice]: 1 },
-    //           $set: { 'recipients.$.responded': true },
-    //           lastResponded: new Date()
-    //         }
-    //       ).exec();
-    //     })
-    //     .value();
+      _.chain(req.body)
+        .map(({ email, url }) => {
+          const match = p.test(new URL(url).pathname);
+          if (match) {
+            return { email, surveyId: match.surveyId, choice: match.choice };
+          }
+        })
+        .compact()
+        .uniqBy('email', 'surveyId')
+        .each(({ surveyId, email, choice }) => {
+          Survey.updateOne(
+            {
+              _id: surveyId,
+              recipients: {
+                $elemMatch: { email: email, responded: false }
+              }
+            },
+            {
+              $inc: { [choice]: 1 },
+              $set: { 'recipients.$.responded': true },
+              lastResponded: new Date()
+            }
+          ).exec();
+        })
+        .value();
   
-    //   res.send({});
-    // });
+      res.send({});
+    });
 
 
     app.post('/api/surveys/webhooks/test', (req, res) => {
